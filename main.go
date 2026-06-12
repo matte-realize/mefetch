64ce04f87b1aa01fc98e21b931c2091c2fa65304
IV <ffenyushi@gmail.com>
2026-06-12 17:30:58 -0400

    Auto open browser

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"runtime"
+	"time"
 
 	"github.com/joho/godotenv"
 	"mefetch/handlers"
@@ -23,7 +26,32 @@ func main() {
 
 	port := os.Getenv("PORT")
 
+	go func() {
+		time.Sleep(500 * time.Millisecond)
+		openBrowser("http://localhost:" + port)
+	}()
+
 	fmt.Printf("Server running on http://localhost:%s\n", port)
 
 	log.Fatal(http.ListenAndServe(":"+port, mux))
+}
+
+func openBrowser(url string) {
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("rundll32", "url.dll,FileProtocolHandler", url)
+	case "darwin":
+		cmd = exec.Command("open", url)
+	case "linux":
+		cmd = exec.Command("xdg-open", url)
+	default:
+		fmt.Println("could not detect OS - open your browser manually at", url)
+		return
+	}
+
+	if err := cmd.Start(); err != nil {
+		fmt.Println("could not open browser:", err)
+	}
 }
